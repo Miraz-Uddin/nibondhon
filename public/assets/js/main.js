3c738fceed6d774d157de3acb5c89588f49e7e39
Md. Miraz Uddin <mirazuddin0623@gmail.com>
2022-03-20 23:01:47 +0600

    Password Validation Done

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -30,7 +30,7 @@
   });
   password_field.addEventListener("input", function (e) {
     e.preventDefault();
-    popupMessage(this.id, false);
+    popupMessage(this.id, passwordValidation(this.value));
   });
   portfolio_field.addEventListener("input", function (e) {
     e.preventDefault();
@@ -100,5 +100,9 @@
 
   function slugValidation(val) {
     return /^[a-z0-9\.\-]{3,}$/.test(val);
+  }
+  
+  function passwordValidation(val) {
+    return /(?=(?:.*\d){1})(?=(?:.*[A-Z]){1})(?=(?:.*[a-z]){1})(?=(?:.*[!@#%$&*]){1})/.test(val);
   }
 })();
