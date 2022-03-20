29a6ee6868f6510f255c7c5b7919cce0f293aad7
Md. Miraz Uddin <mirazuddin0623@gmail.com>
2022-03-20 21:02:32 +0600

    UI done

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -8,22 +8,6 @@
         isEmpty(valueList); 
     });
 
-    function emptyFieldCheck(arr){
-        let problemIndex = -1;
-        let booleanVal = false;
-        for(let p=0; p<arr.length ;p++){
-            if(Array.from(arr[p].children[1].value).length == 0){
-                booleanVal = true;
-                problemIndex = p;
-                break;   
-            }
-        }
-        return {
-            bool: booleanVal,
-            index: problemIndex
-        }
-    }
-
     function isEmpty(valueList){
         let onlyInputs = valueList.filter(obj=>{
             if(obj.children[1] == 'undefined' || obj.children[1] == null)
@@ -33,10 +17,18 @@
         onlyInputs.map(x=>{
             if(Array.from(x.children[1].classList).includes('has-bug')) x.children[1].classList.remove('has-bug');
         });
-        if(emptyFieldCheck(onlyInputs).bool){
-            const first_error = emptyFieldCheck(onlyInputs).index;
-            valueList[first_error].children[1].classList.add('has-bug');
+
+        let problemIndex = -1;
+        let booleanVal = false;
+        for(let p=0; p<onlyInputs.length ;p++){
+            if(Array.from(onlyInputs[p].children[1].value).length == 0){
+                booleanVal = true;
+                problemIndex = p;
+                break;   
+            }
         }
+        if(booleanVal)
+            valueList[problemIndex].children[1].classList.add('has-bug');
     }
 
   })()
