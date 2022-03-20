f4c0782e53de21dbae980d2b7cffaafbae37cbaf
Md. Miraz Uddin <mirazuddin0623@gmail.com>
2022-03-20 20:40:34 +0600

    Prevent the form if any field is Empty

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,43 @@
 (function () {
-  
-})();
+    
+    const main_form = document.querySelector('#main_form');
+
+    main_form.addEventListener('submit',function(e){
+        e.preventDefault();
+        let valueList = Array.from(e.target.children);
+        isEmpty(valueList); 
+    });
+
+    function emptyFieldCheck(arr){
+        let problemIndex = -1;
+        let booleanVal = false;
+        for(let p=0; p<arr.length ;p++){
+            if(Array.from(arr[p].children[1].value).length == 0){
+                booleanVal = true;
+                problemIndex = p;
+                break;   
+            }
+        }
+        return {
+            bool: booleanVal,
+            index: problemIndex
+        }
+    }
+
+    function isEmpty(valueList){
+        let onlyInputs = valueList.filter(obj=>{
+            if(obj.children[1] == 'undefined' || obj.children[1] == null)
+                return false;
+            return true;
+        });
+        onlyInputs.map(x=>{
+            if(Array.from(x.children[1].classList).includes('has-bug')) x.children[1].classList.remove('has-bug');
+        });
+        if(emptyFieldCheck(onlyInputs).bool){
+            const first_error = emptyFieldCheck(onlyInputs).index;
+            valueList[first_error].children[1].classList.add('has-bug');
+        }
+    }
+
+  })()
+
